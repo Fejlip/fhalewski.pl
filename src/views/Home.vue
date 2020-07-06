e2280eb54520e95d42932f5056fc7625270565f6
Fejlip <fejlip@pop-os.localdomain>
2020-07-06 21:23:27 +0200

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <v-container fluid fill-height class="home-hero">
+        <v-layout justify-center align-left column pa-5>
+            <div class="heading display-4 font-weight-black white--text text-xs-center">HEALTHY MEALS</div>
+            <div class="heading display-4 font-weight-black white--text text-xs-center mb-3">FOR YOUR TABLE</div>
+            <div class="display-1 font-weight-bold white--text text-xs-center">Finally be a foodie at home with fresh, chef-prepared meals delivered daily to your door.</div>
+            <v-btn fab class="mt-5 brown darken-4">
+            <v-icon large color="white">mdi-heart</v-icon>
+            </v-btn>
+        </v-layout>
+    </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
+    name: 'HomeHero'
 };
 </script>
+
+<style scoped>
+.home-hero {
+    background: url('http://source.unsplash.com/0BhSKStVtdM');
+    background-size: cover;
+
+}
+
+.heading {
+  text-shadow: 2px 2px 3px rgba(33,33,33,0.5);
+}
+</style>
